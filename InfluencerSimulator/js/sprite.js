40594a047854548c983d19620a08b763230d585f
Doomsdaycie <101742689+Doomsdaycie@users.noreply.github.com>
2022-06-06 10:17:16 -0400

    Delete InfluencerSimulator directory

--- InfluencerSimulator/js/sprite.js
+++
@@ -1,9 +0,0 @@
-class Sprite{
- constructor(img, largeur, hauteur, nbrImagesHoriz, nbreImagesVert){
- this.image = img
- this.largeur = largeur
- this.hauteur = hauteur
- this.nbrImagesHoriz = nbrImagesHoriz
- this.nbreImagesVert = nbreImagesVert
- }
-}
